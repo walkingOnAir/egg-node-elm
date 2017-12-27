2498ae3828b9c882f57bf37e475a1e09fe8d2ef7
wangpeng <747610383@qq.com>
2017-12-27 21:23:45 +0800

    admin service and unit

--- app/service/admin/admin.js
+++ app/service/admin/admin.js
@@ -23,6 +23,46 @@ class AdminService extends Service {
   async findOneByUsername(username) {
     return this.ctx.model.Admin.Admin.find({ user_name: username });
   }
+  
+  /**
+   * 分页获取管理员信息
+   * @param limit
+   * @param offset
+   * @returns {Promise.<*>}
+   */
+  async getAdminList(limit = 20, offset = 0) {
+    return this.ctx.model.Admin.Admin.find({}, {_id: 0, password: 0})
+      .sort({id: -1})
+      .skip(Number(offset))
+      .limit(Number(limit));
+  }
+  
+  /**
+   * 获取管理员数量
+   * @returns {Promise.<*|{type, default}>}
+   */
+  async getAdminCount() {
+    return this.ctx.model.Admin.Admin.count();
+  }
+  
+  /**
+   * 根据id获取管理员信息
+   * @param id
+   * @returns {Promise.<T>}
+   */
+  async getAdminInfoById(id) {
+    return this.ctx.model.Admin.Admin.find({id}, {_id: 0, __v: 0, password: 0});
+  }
+  
+  /**
+   * 根据id更新管理员头像
+   * @param id
+   * @param avatar
+   * @returns {Promise.<*>}
+   */
+  async updateAdminAvatar(id, avatar) {
+    return this.ctx.model.Admin.Admin.findOneAndUpdate({id}, {$set: {avatar}});
+  }
 }
 
 module.exports = AdminService;
--- test/app/service/admin/admin.test.js
+++ test/app/service/admin/admin.test.js
@@ -25,13 +25,55 @@ describe('test/app/service/admin/admin.test.js', () => {
     });
   });
 
-  describe('find()', () => {
+  describe('findOneByUsername()', () => {
     it('should get an admin user', async () => {
       // 创建ctx
       const ctx = app.mockContext();
       // 通过ctx访问到service
       const adminUser = await ctx.service.admin.admin.findOneByUsername('admin');
       assert(adminUser);
+    });
+  });
+  
+  describe('getAdminList()', () => {
+    it('should get admin user list', async () => {
+      // 创建ctx
+      const ctx = app.mockContext();
+      // 通过ctx访问到service
+      const adminUserList = await ctx.service.admin.admin.getAdminList();
+      assert(adminUserList);
+      assert(adminUserList[0].id === 1);
+    });
+  });
+  
+  describe('getAdminCount()', () => {
+    it('should get admin count', async () => {
+      // 创建ctx
+      const ctx = app.mockContext();
+      // 通过ctx访问到service
+      const adminUserCount = await ctx.service.admin.admin.getAdminCount();
+      assert(adminUserCount > 1);
+    });
+  });
+  
+  describe('getAdminInfoById()', () => {
+    it('should get admin info by id', async () => {
+      // 创建ctx
+      const ctx = app.mockContext();
+      // 通过ctx访问到service
+      const adminUser = await ctx.service.admin.admin.getAdminInfoById(1);
+      assert(adminUser);
+    });
+  });
+  
+  describe('updateAdminAvatar()', () => {
+    it('should update admin avatar by id', async () => {
+      // 创建ctx
+      const ctx = app.mockContext();
+      // 通过ctx访问到service
+      const adminUser = await ctx.service.admin.admin.updateAdminAvatar(1, 'update.jpg');
+      assert(adminUser.id === 1);
+      assert(adminUser.avatar === 'update.jpg');
     });
   });
 });
